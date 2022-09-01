b9608e7a8ae716c430dde01a2967fe20b15a90c0
Lictor Guzman <lctrgzmn@gmail.com>
2022-09-01 02:36:32 -0500

    exporting all submodules

--- src/PodCatcher.hs
+++ src/PodCatcher.hs
@@ -1,4 +1,9 @@
-module PodCatcher where
+module PodCatcher (
+  module PodCatcher.Database,
+  module PodCatcher.Download,
+  module PodCatcher.ParseXML,
+  module PodCatcher.Types
+) where
 
 import           PodCatcher.Database
 import           PodCatcher.Download
